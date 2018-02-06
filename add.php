9ae4e2d38723a315bc2a1dd9e466f0fe26992210
tarun20mogra <tarun20mogra@gmail.com>
2018-02-06 14:28:49 -0800

    completed all the add functions

--- add.php
+++ add.php
@@ -1,15 +1,64 @@
 <?php
 require_once('connection.php');
-$artistName = $_POST['artistName'];
-$birthplace = $_POST['birthplace'];
-$age = $_POST['age'];
-$style = $_POST['style'];
 
-$res=mysqli_query($connection,"INSERT INTO artist (AName,birthplace,age,style) VALUES ('$artistName','$birthplace','$age','$style')");
-if(!$res){
-     echo 'not inserted';
+if($_POST['addToArtist']){
+
+    $artistName = $_POST['artistName'];
+    $birthplace = $_POST['birthplace'];
+    $age = $_POST['age'];
+    $style = $_POST['style'];
+
+    $res=mysqli_query($connection,"INSERT INTO artist (AName,birthplace,age,style) VALUES ('$artistName','$birthplace','$age','$style')");
+    if(!$res){
+        echo "<script>window.alert('not inserted');</script>";
+        echo"<script>window.location.href = 'artist.php';</script>";
+    }
+    else{
+        echo "<script>window.alert('inserted');</script>";
+        echo"<script>window.location.href = 'artist.php';</script>";
+    }
+
 }
-else{
-    echo 'inserted';
+
+
+if($_POST['addToArtwork']){
+
+    $title = $_POST['title'];
+    $year = $_POST['year'];
+    $type = $_POST['type'];
+    $price = $_POST['price'];
+    $artistName = $_POST['artistName'];
+
+    $res=mysqli_query($connection,"INSERT INTO artwork (title,year,type,price,AName) VALUES ('$title','$year','$type','$price','$artistName')");
+    if(!$res){
+        echo "<script>window.alert('not inserted');</script>";
+        echo"<script>window.location.href = 'artist.php';</script>";
+    }
+    else{
+        echo "<script>window.alert('inserted');</script>";
+        echo"<script>window.location.href = 'artist.php';</script>";
+    }
+
 }
+
+if($_POST['addToCustomer']){
+
+    $customerID = $_POST['customerID'];
+    $costumerName = $_POST['costumerName'];
+    $adress = $_POST['adress'];
+    $amount = $_POST['amount'];
+
+    $res=mysqli_query($connection,"INSERT INTO customer (custId,CName,address,amount) VALUES ('$customerID','$costumerName','$adress','$amount')");
+    if(!$res){
+        echo "<script>window.alert('not inserted');</script>";
+        echo"<script>window.location.href = 'artist.php';</script>";
+    }
+    else{
+        echo "<script>window.alert('inserted');</script>";
+        echo"<script>window.location.href = 'artist.php';</script>";
+    }
+
+}
+
+
 ?>
--- artist.php
+++ artist.php
@@ -13,16 +13,40 @@ require_once('connection.php');
 
 <body>
     <div>
-
         <h1>Do an add to the databases</h1>
         <p>Click to see the options</p>
+
         <form action="add.php" method="post">
-            <h3>Add to Artist:</h3>
-            <label>Name:</label><input type="text" placeholder="name of the artist" name="artistName">
+            <h3>Add to Artist table:</h3>
+            <input type="hidden" name="addToArtist" value="addToArtist">
+            <label>Artist Name:</label><input type="text" name="artistName">
             <label>Birthplace:</label><input type="text" name="birthplace">
             <label> Age: </label><input type="text" name="age">
             <label>Style: </label><input type="text" name="style">
+            <button type="submit" name="add" value="add" >Add</button>
 
+
+        </form>
+        <form action="add.php" method="post">
+
+            <h3>Add to Artwork table:</h3>
+            <input type="hidden" name="addToArtwork" value="addToArtwork">
+            <label>Title:</label><input type="text" name="title">
+            <label>Year:</label><input type="text" name="year">
+            <label> Type: </label><input type="text" name="type">
+            <label>Price: </label><input type="text" name="price">
+            <label>Artist Name: </label><input type="text" name="artistName">
+            <button type="submit" name="add" value="add" >Add</button>
+
+
+        </form>
+        <form action="add.php" method="post" >
+            <h3>Add to Customer</h3>
+            <input type="hidden" name="addToCustomer" value="addToCustomer">
+            <label>Customer ID:</label><input type="text" name="customerID">
+            <label>Costumer Name:</label><input type="text" name="costumerName">
+            <label> Adress: </label><input type="text" name="adress">
+            <label>Amount: </label><input type="text" name="amount">
             <button type="submit" name="add" value="add" >Add</button>
 
 
