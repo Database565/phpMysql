3ef5c2bafc7e02889671ea6801484afefa630d54
tarun20mogra <tarun20mogra@gmail.com>
2018-02-07 18:55:13 -0800

    select from table

--- artist.php
+++ artist.php
@@ -20,6 +20,15 @@ require_once('connection.php');
         padding-bottom: 5px;
         padding-top: 5px;
     }
+    .getButton{
+        margin-bottom: 5px;
+        margin-top: 5px;
+        min-width: 300px;
+    }
+    .tablePrint{
+        margin-top: 5px;
+        margin-bottom: 5px;
+    }
 </style>
 <body>
     <div class="container">
@@ -116,15 +125,152 @@ require_once('connection.php');
                         </div>
                     </div>
                 </div>
+
+                <!-- Select data from table -->
                 <div class="card">
                     <div class="card-header">
                         <a class="collapsed card-link" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-                            Collapsible Group Item #2
+                            Click here to see the database tables
                         </a>
                     </div>
                     <div id="collapseTwo" class="collapse">
                         <div class="card-body">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+                            <button type="button" class="btn btn-secondary getButton" data-toggle="collapse" data-target="#getArtist" name="artist">Get the list of artist</button>
+                            <br>
+                            <div id="getArtist" class="collapse tablePrint">
+
+                                <?php
+                                $resultArtist = mysqli_query($connection,'SELECT * FROM artist');
+                                echo "<table border='1'>";
+                                echo"<tr>";
+                                echo "<th>AName</th>";
+                                echo"<th>Birthplace</th>";
+                                echo"<th>Age</th>";
+                                echo"<th>Style</th>";
+                                echo"</tr>";
+                                while($row = mysqli_fetch_array($resultArtist))
+                                {
+                                    echo "<tr>";
+                                    echo "<td>" . $row['AName'] . "</td>";
+                                    echo "<td>" . $row['birthplace'] . "</td>";
+                                    echo "<td>" . $row['age'] . "</td>";
+                                    echo "<td>" . $row['style'] . "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</table>";
+                                ?>
+
+
+                            </div>
+                            <button type="button" class="btn btn-secondary getButton" data-toggle="collapse" data-target="#getArtwork" name="artwork">Get the list of the artwork</button>
+                            <br>
+                            <div id="getArtwork" class="collapse tablePrint">
+                                <?php
+                                $resultArtist = mysqli_query($connection,'SELECT * FROM artwork');
+                                echo "<table border='1'>";
+                                echo"<tr>";
+                                echo "<th>Title</th>";
+                                echo"<th>Year</th>";
+                                echo"<th>Type</th>";
+                                echo"<th>Price</th>";
+                                echo"<th>Artist Name</th>";
+                                echo"</tr>";
+                                while($row = mysqli_fetch_array($resultArtist))
+                                {
+                                    echo "<tr>";
+                                    echo "<td>" . $row['title'] . "</td>";
+                                    echo "<td>" . $row['year'] . "</td>";
+                                    echo "<td>" . $row['type'] . "</td>";
+                                    echo "<td>" . $row['price'] . "</td>";
+                                    echo "<td>" . $row['AName'] . "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</table>";
+                                ?>
+                            </div>
+                            <button type="button" class="btn btn-secondary getButton" data-toggle="collapse" data-target="#getCustomer" name="customer">Get the list of the customer</button>
+                            <br>
+                            <div id="getCustomer" class="collapse tablePrint">
+                                <?php
+                                $resultArtist = mysqli_query($connection,'SELECT * FROM customer');
+                                echo "<table border='1'>";
+                                echo"<tr>";
+                                echo "<th>Customer Id</th>";
+                                echo"<th>Customer Name</th>";
+                                echo"<th>Address</th>";
+                                echo"<th>Amount</th>";
+
+                                echo"</tr>";
+                                while($row = mysqli_fetch_array($resultArtist))
+                                {
+                                    echo "<tr>";
+                                    echo "<td>" . $row['custId'] . "</td>";
+                                    echo "<td>" . $row['CName'] . "</td>";
+                                    echo "<td>" . $row['address'] . "</td>";
+                                    echo "<td>" . $row['amount'] . "</td>";
+
+                                    echo "</tr>";
+                                }
+                                echo "</table>";
+                                ?>
+
+
+                            </div>
+                            <button type="button" class="btn btn-secondary getButton" data-toggle="collapse" data-target="#getClassify" name="classify">Get the list of classify entries</button>
+                            <br>
+                            <div id="getClassify" class="collapse tablePrint">
+                                <?php
+                                $resultArtist = mysqli_query($connection,'SELECT * FROM classify');
+                                echo "<table border='1'>";
+                                echo"<tr>";
+                                echo "<th>Title</th>";
+                                echo"<th>Group Name</th>";
+
+
+                                echo"</tr>";
+                                while($row = mysqli_fetch_array($resultArtist))
+                                {
+                                    echo "<tr>";
+                                    echo "<td>" . $row['title'] . "</td>";
+                                    echo "<td>" . $row['GName'] . "</td>";
+
+
+                                    echo "</tr>";
+                                }
+                                echo "</table>";
+                                ?>
+
+
+
+                            </div>
+                            <button type="button" class="btn btn-secondary getButton" data-toggle="collapse" data-target="#getGroupEntries" name="groupEntries">Get the list of like group entries</button>
+                            <br>
+                            <div id="getGroupEntries" class="collapse tablePrint">
+                                <?php
+                                $resultArtist = mysqli_query($connection,'SELECT GName FROM group');
+                                echo "<table border='1'>";
+                                echo"<tr>";
+                                echo"<th>Group Name</th>";
+                                echo"</tr>";
+                                while($row = mysqli_fetch_array($resultArtist))
+                                {
+                                    echo "<tr>";
+                                    echo "<td>" . $row['GName'] . "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</table>";
+                                ?>
+
+
+                            </div>
+                            <button type="button" class="btn btn-secondary getButton" data-toggle="collapse" data-target="#getArtistEntries" name="artistEntries">Get the list of like artist entries</button>
+                            <div id="getArtistEntries" class="collapse">
+                                <p>here i will write to get the artist entries</p>
+
+
+                            </div>
+
                         </div>
                     </div>
                 </div>
